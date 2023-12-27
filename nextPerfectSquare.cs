35325c285439c63b210503e43b832f6c34c25cdb
Nebil1 <nebilyk@gmail.com>
2023-12-27 10:23:38 +0300

    Add nextPerfectSquare.cs

---
+++ nextPerfectSquare.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static int nextPerfectSquare(int n)
+    {
+        if (n < 0) return 0;
+        int root = (int)Math.Floor(Math.Sqrt(n)) + 1;
+        return root * root;
+    }
+}
